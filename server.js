3350e8c20ef99db4fe0c90b33cc2f10cab19c8dd
Emily Bacon <em222ilyb@gmail.com>
2023-03-11 21:29:25 -0800

    updating

--- server.js
+++ server.js
@@ -2,6 +2,19 @@
 const express = require('express')
 const app = express()
 
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+
+
+
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
